ccf2106450f1c50a1468777d1849aafd790641d2
alione10-lm <aliouaneoussama89@gmail.com>
2026-02-24 11:26:16 +0000

    remove global error handler

--- server.js
+++ server.js
@@ -14,11 +14,7 @@ app.get("/", (req, res) => {
 app.use("/users", userRouter);
 app.use("/wallets", walletRouter);
 
-app.use((req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  error.statusCode = 404;
-  next(error); // Sift l'error l'middleware lli l-te7t
-});
+
 
 app.listen(3000, () => {
   console.log("server running on port 3000");
